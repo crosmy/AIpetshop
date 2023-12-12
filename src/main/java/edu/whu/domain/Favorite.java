5b231ce782e048ecfeac05b0c55b5e074582fe53
eaglemouth <eaglemouth@qq.com>
2023-12-12 23:59:02 +0800

    Modify structure

--- src/main/java/edu/whu/domain/Favorite.java
+++
@@ -1,9 +0,0 @@
-package edu.whu.domain;
-
-import lombok.Data;
-
-@Data
-public class Favorite {
-    private Integer userId;
-    private Integer postId;
-}
--- src/main/java/edu/whu/service/IRatingsService.java
+++
@@ -1,9 +0,0 @@
-package edu.whu.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import edu.whu.domain.Ratings;
-import edu.whu.exception.CustomException;
-
-public interface IRatingsService extends IService<Ratings> {
-    void ratePost(Integer postId, Integer star, Integer userId) throws CustomException;
-}
